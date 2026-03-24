5357a155e03b61100e3dc14e3baf3ffb04c3b55c
rafaDev <rrg1459@hotmail.com>
2026-03-24 16:26:06 -0400

    Enhance quote display by adding quote type and updating layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ const initialQuoteState = getInitialQuoteState()
 function App() {
   const [{ randomQuote }] = useState(initialQuoteState)
 
-  const { text, author } = randomQuote;
+  const { text, author, group } = randomQuote;
+  const quoteType = group === 1
+    ? 'Inspiracional'
+    : group === 2
+      ? 'Trading'
+      : 'Tipo no clasificado';
 
   return (
-    <>
-      <p>Texto: {text}</p>
-      <p>Autor: {author}</p>
-    </>
+    <main>
+      <h4 id="quote-text">"{text}"</h4>
+      <p id="quote-author">{author}</p>
+      <p id="quote-type">({quoteType})</p>
+    </main>
   )
 }
 
